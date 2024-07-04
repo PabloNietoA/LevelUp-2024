79967c2d7a9a22ad27d121a174ff54516f475dc5
Carlos <56149047+carlox1998@users.noreply.github.com>
2024-07-04 16:09:13 +0200

    Squashed commit of the following:
    commit 558485ac159b5db2142bdc620d1115a57d18df97
    Merge: 19120a3 ce94fee
    Author: Carlos <56149047+carlox1998@users.noreply.github.com>
    Date:   Thu Jul 4 12:14:08 2024 +0200
    
        Merge branch 'Menu' of https://github.com/PabloNietoA/LevelUp-2024 into Menu
    
    commit ce94fee09098b975174ad1d102c4583d88bd08c0
    Author: BatlloseraDev <86764975+BatlloseraDev@users.noreply.github.com>
    Date:   Thu Jul 4 11:11:58 2024 +0200
    
        mensajes
    
        Los mensajes se actualizan correctamente, desplazando tambien la barra  y la posicion en el chat en el que se encuentra
    
    commit f7c2684562af769e94d9d2bd7903ef6400cdcf87
    Author: BatlloseraDev <batlloseradeveloper@gmail.com>
    Date:   Wed Jul 3 21:19:47 2024 +0200
    
        Botones y Manager
    
        Ahora reconoce cuando clicqueas un boton y se conecta con el manager
    
    commit 19120a32bd4a7844dc28ee15e3060b2d3ede89ed
    Merge: 7ca7016 e8ccd44
    Author: Carlos <56149047+carlox1998@users.noreply.github.com>
    Date:   Wed Jul 3 19:19:53 2024 +0200
    
        Merge branch 'Menu' of https://github.com/PabloNietoA/LevelUp-2024 into Menu
    
    commit e8ccd4493ccba754191f88ba6b29fcfedc110965
    Author: BatlloseraDev <batlloseradeveloper@gmail.com>
    Date:   Wed Jul 3 19:17:08 2024 +0200
    
        GameManager
    
        Game manager para generar los cuadros de texto
    
    commit 7307a142483b2004b6e09dff41c0dff36f67ca16
    Author: BatlloseraDev <batlloseradeveloper@gmail.com>
    Date:   Wed Jul 3 18:40:27 2024 +0200
    
        CuadrosDeTextoPrefab
    
        Los cuadros de texto preparados para prefab
    
    commit e683ce8af3ca48e4cac312ba3685eba3c94d9f77
    Author: BatlloseraDev <batlloseradeveloper@gmail.com>
    Date:   Wed Jul 3 17:02:45 2024 +0200
    
        CuadrosDeTextos2
    
        Mejorado el diseño y funcionamiento de los contenidos de como se debe crear mensajes
    
    commit af940b8e4ba3b8fe11a7ec11722ce48e1ba1469e
    Author: BatlloseraDev <batlloseradeveloper@gmail.com>
    Date:   Tue Jul 2 22:22:03 2024 +0200
    
        Dialogos de Texto1
    
        Inicio del desarrollo de la aparicion de dialogos de texto dentro del canva

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,19 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-  
+    [Header("Mensajes Prefabs")]
     [SerializeField] private GameObject EsbirroMensajePrefab;
+    [SerializeField] private GameObject EsbirroMensajeVacioPrefab;
     [SerializeField] private GameObject RespuestasBotonPrefab;
+    [SerializeField] private GameObject RespuestasVacioPrefab;
+    [SerializeField] private GameObject RespuestaPrefab;
+
+    [Header("Canvas donde instanciar")]
+    [SerializeField] private GameObject ContenidoMensajesEsbirros;
+    [SerializeField] private GameObject ContenidoMensajesNuestros;
+
+    //Guardar el dato de la pregunta encontrada
+    private int NumeroPregunta=0;
 
     private List<Dictionary<string,string>> questionData = new List<Dictionary<string,string>>();
     private string[] keys;
@@ -20,19 +30,21 @@ public class GameManager : MonoBehaviour
     private int questionIndex;
 
     Resources sinews;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         LoadCSV();
         questionIndex = GetLine();
         LoadCSVLine(questionIndex);
+        GenerarPreguntaYRespuestas();
+        ObjetoContenidos = GameObject.FindGameObjectWithTag("Content");
     }
 
-    // Update is called once per frame
+    // Update is called once per frame   
     void Update()
     {
-        
+
     }
 
     //Funcion que cargara el CSV entero
@@ -87,11 +99,72 @@ public class GameManager : MonoBehaviour
     }
 
     void ChargeNewMessage()
-    { 
+    {
         Dictionary<string, string> entry = questionData[questionIndex];
         string question = entry["Pregunta"];
         questionText.text = question;
     }
+
+
+    void GenerarPreguntaYRespuestas()
+    {
+
+        //Esbirro
+        GameObject ObjetoTemporal = Instantiate(EsbirroMensajePrefab);//texto (Aun queda por cambiar el texto, se cambia siguiente a este)
+        ObjetoTemporal.transform.parent = ContenidoMensajesEsbirros.transform;
+        ObjetoTemporal = Instantiate(EsbirroMensajeVacioPrefab); //Vacio
+        ObjetoTemporal.transform.parent = ContenidoMensajesEsbirros.transform;
+        //Nuestro
+        ObjetoTemporal = Instantiate(RespuestasVacioPrefab); //Vacio
+        ObjetoTemporal.transform.parent = ContenidoMensajesNuestros.transform;
+        ObjetoTemporal = Instantiate(RespuestasBotonPrefab); //texto (Aun queda por cambiar el texto, se cambia siguiente a este)
+        ObjetoTemporal.transform.parent = ContenidoMensajesNuestros.transform;
+
+    }
+
+
+    public void EliminarBotonesYGenerarTexto(int numeroRespuesta)
+    {
+        Transform parentTransform = ContenidoMensajesNuestros.transform;
+
+
+        //Aqui cojo el hijo y lo elimino
+        int numeroHijo = parentTransform.childCount - 1;
+        Destroy(parentTransform.GetChild(numeroHijo).gameObject);
+
+        //aqui en seria utilizar el numeroRespuesta y sacar el texto apropiado
+        GameObject ObjetoTemporal = Instantiate(RespuestaPrefab);
+        ObjetoTemporal.transform.parent = ContenidoMensajesNuestros.transform;
+        AplicarEstadisticas();
+
+        //cargar de nuevo los mensajes
+
+        GenerarPreguntaYRespuestas();
+
+        //aplicar movimiento al contenido
+
+        ObjetoContenidos.GetComponent<RefreshHeighttByContent>().ChangePositionY();
+
+
+    }
+
+    void AplicarEstadisticas()
+    {
+        //por hacer
+
+
+    }
+
+
+
+
+
+    /*
+    Numero random para coger pregunta random del csv
+    Coger este texto para meterselo a la pregunta instanciada
+    */
+
+
 
 
 }
