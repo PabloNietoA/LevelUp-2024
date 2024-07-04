c902a6b2611a835c5de93c6c682706888637b907
Carlos <56149047+carlox1998@users.noreply.github.com>
2024-07-04 17:56:36 +0200

    Add text to TMP
    Add text from CSV to TMP

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject RespuestaPrefab;
 
     [Header("Canvas donde instanciar")]
+    
+    [SerializeField] private GameObject ObjetoContenidos;
     [SerializeField] private GameObject ContenidoMensajesEsbirros;
     [SerializeField] private GameObject ContenidoMensajesNuestros;
 
@@ -23,20 +25,26 @@ public class GameManager : MonoBehaviour
     private List<Dictionary<string,string>> questionData = new List<Dictionary<string,string>>();
     private string[] keys;
 
-    [SerializeField] TextMeshProUGUI questionText;
-    TextMeshProUGUI answer1;
-    TextMeshProUGUI answer2;
+    string questionText;
+    string shortAnswerText1;
+    string shortAnswerText2;
+    string answerText1;
+    string answerText2;
 
     private int questionIndex;
 
     Resources sinews;
+
+    private void Awake() {
+        sinews = FindObjectOfType<Resources>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         LoadCSV();
         questionIndex = GetLine();
-        LoadCSVLine(questionIndex);
+        //LoadCSVLine(questionIndex);
         GenerarPreguntaYRespuestas();
         ObjetoContenidos = GameObject.FindGameObjectWithTag("Content");
     }
@@ -77,6 +85,10 @@ public class GameManager : MonoBehaviour
         return Random.Range(1,questionData.Count);
     }
 
+    
+    /*
+    Numero random para coger pregunta random del csv
+    Coger este texto para meterselo a la pregunta instanciada
     //Funcion que cargara una Linea del CSV especificada
     void LoadCSVLine (int lineNumber){
         if(lineNumber < questionData.Count){
@@ -97,28 +109,48 @@ public class GameManager : MonoBehaviour
         }
 
     }
+    */
 
     void ChargeNewMessage()
     {
         Dictionary<string, string> entry = questionData[questionIndex];
-        string question = entry["Pregunta"];
-        questionText.text = question;
+        questionText = entry["Pregunta"];
+    }
+
+    void ChargeAnswers()
+    {
+        Dictionary<string, string> entry = questionData[questionIndex];
+        shortAnswerText1 = entry["Respuesta1"];
+        shortAnswerText2 = entry["Respuesta2"];
+        answerText1 = entry["Respuesta1"];
+        answerText2 = entry["Respuesta2"];
     }
 
 
     void GenerarPreguntaYRespuestas()
     {
+        ChargeNewMessage();
+        ChargeAnswers();
+
 
         //Esbirro
         GameObject ObjetoTemporal = Instantiate(EsbirroMensajePrefab);//texto (Aun queda por cambiar el texto, se cambia siguiente a este)
         ObjetoTemporal.transform.parent = ContenidoMensajesEsbirros.transform;
+        ObjetoTemporal.GetComponentInChildren<TMP_Text>().text = questionText;
+
         ObjetoTemporal = Instantiate(EsbirroMensajeVacioPrefab); //Vacio
         ObjetoTemporal.transform.parent = ContenidoMensajesEsbirros.transform;
+
+
         //Nuestro
         ObjetoTemporal = Instantiate(RespuestasVacioPrefab); //Vacio
         ObjetoTemporal.transform.parent = ContenidoMensajesNuestros.transform;
+
         ObjetoTemporal = Instantiate(RespuestasBotonPrefab); //texto (Aun queda por cambiar el texto, se cambia siguiente a este)
         ObjetoTemporal.transform.parent = ContenidoMensajesNuestros.transform;
+        TMP_Text[] text= ObjetoTemporal.GetComponentsInChildren<TMP_Text>();
+        text[0].text= answerText1;
+        text[1].text= answerText2;
 
     }
 
@@ -135,7 +167,15 @@ public class GameManager : MonoBehaviour
         //aqui en seria utilizar el numeroRespuesta y sacar el texto apropiado
         GameObject ObjetoTemporal = Instantiate(RespuestaPrefab);
         ObjetoTemporal.transform.parent = ContenidoMensajesNuestros.transform;
-        AplicarEstadisticas();
+        if(numeroRespuesta == 1){
+            ObjetoTemporal.GetComponentInChildren<TMP_Text>().text = answerText1;
+        }else{
+            ObjetoTemporal.GetComponentInChildren<TMP_Text>().text = answerText2;
+        }           
+        
+
+        ModifyValues(questionIndex,numeroRespuesta);
+        questionIndex = GetLine();
 
         //cargar de nuevo los mensajes
 
@@ -148,21 +188,49 @@ public class GameManager : MonoBehaviour
 
     }
 
-    void AplicarEstadisticas()
-    {
-        //por hacer
+string[] ModifyValues(int questionIndex, int response){
+        Dictionary<string, string> entry = questionData[questionIndex];
+        string[] values = new string[3];
+        Debug.Log("Respuesta: "+response);
+        Debug.Log("OK");
+        if(response == 1){
+            Debug.Log("OKKKKKK");
+            //Valores de la Respuesta 1
+            values[0] = entry["ValorF1"];
+            values[1] = entry["ValorL1"];
+            values[2] = entry["ValorI1"];
+            //values[3] = entry["ValorA1"];
 
+            //return values;
+        }else if(response == 2){
+            Debug.Log("Noooooo");
+            //Valores de la Respuesta 2
+            values[0] = entry["ValorF2"];
+            values[1] = entry["ValorL2"];
+            values[2] = entry["ValorI2"];
+            //values[3] = entry["ValorA1"];
+            
+            //return values;
+        }else{
+            Debug.Log("yeyeyeyeye");
+            //Valores de Respuesta Random
+            values[0] = entry["ValorF1"];
+            values[1] = entry["ValorL2"];
+            values[2] = entry["ValorI2"];
+            //values[3] = entry["ValorA1"];
 
+            //return values;
+        }
+        sinews.ModifyResource(values);
+        sinews.ShowResources();
+
+        return values;
     }
 
 
 
 
 
-    /*
-    Numero random para coger pregunta random del csv
-    Coger este texto para meterselo a la pregunta instanciada
-    */
 
 
 
